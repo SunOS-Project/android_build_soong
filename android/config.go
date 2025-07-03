8a83260c79197384779a9c0088a449c93c6b4bb8
chandu078 <chandudyavanapelli03@gmail.com>
2025-07-03 13:56:52 +0530

    Update signing keys path
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- android/config.go
+++ android/config.go
@@ -1038,7 +1038,7 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
-	if strings.HasPrefix(defaultCert, "vendor/sun-priv/") {
+	if strings.HasPrefix(defaultCert, "certs/") {
 		return "release-keys"
 	}
 	return "dev-keys"
